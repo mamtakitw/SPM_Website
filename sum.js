9b07910294360665a1223f3d6c57479b41f39b05
Mamta Kumari Gupta <72861695+mamtakitw@users.noreply.github.com>
2024-12-29 13:52:40 +0530

    Add files via upload

---
+++ sum.js
@@ -0,0 +1,8 @@
+//javaScript program to add two numbers 
+//define variable with integers
+var num1=2
+var num2=3
+// add two numbers
+var sum=num1+num2;
+//display output
+console.log("the sum of " +num1+"and" +num2+"is:" +sum);
